c5dcccbf813626fd0462f90502b34ea2f59d410c
Moaesaycto <stephen.lerantges.los@gmail.com>
2026-05-02 19:02:27 +1000

    Fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
+  build: { outDir: 'dist' },
   plugins: [
     tailwindcss(),
+    react(),
   ],
-   resolve: {
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },

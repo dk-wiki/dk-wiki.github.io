d46aaeeb7a91f54363453d35b84576a9b0d58cbd
Moaesaycto <stephen.lerantges.los@gmail.com>
2026-05-02 18:58:14 +1000

    Fixed font stuff

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
-import "@fontsource/quicksand"; 
-
-import '@fontsource/source-sans-3';
+import "@fontsource/quicksand/400.css";
+import '@fontsource/source-sans-3/400.css';
 import '@fontsource/source-sans-3/700.css';
 import '@fontsource/source-sans-3/400-italic.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <App />
+    <App />
   </StrictMode>,
 )
